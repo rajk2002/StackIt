641aa9555b5a7d6a9f1296c75ec1c241a5935580
Raj Khatri <rajkhatri3002@gmail.com>
2025-07-12 04:36:24 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+// server/server.js
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Simple test route
+app.get("/", (req, res) => res.send("StackIt API is running"));
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.error("MongoDB error:", err));
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
